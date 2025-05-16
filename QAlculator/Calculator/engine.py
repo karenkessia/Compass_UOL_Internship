3ebc229dae2ea9fc0d87431ed9c15ab1754b2dc9
Karen <karenkessia16@gmail.com>
2025-05-15 21:31:09 -0400

    feat: implementa método dividir com tratamento de erro

--- QAlculator/Calculator/engine.py
+++ QAlculator/Calculator/engine.py
@@ -7,3 +7,9 @@ def subtrair(self, a, b):
 
     def multiplicar(self, a, b):
       return a * b
+    
+    def dividir(self, a, b):
+     if b == 0:
+        raise ValueError("Não é possível dividir por zero.")
+    return a / b
+
